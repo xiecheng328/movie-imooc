0133598d9fd29ae6f61ecae2d56e19a990557156
xiecheng328 <xiecheng2006@yeah.net>
2019-04-20 10:14:10 +0800

    imooc movie demo

---
+++ cloudfunctions/movielist/index.js
@@ -0,0 +1,20 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+var rp = require('request-promise');
+
+// 云函数入口函数
+exports.main = async(event, context) => {
+
+
+  return rp(`http://api.douban.com/v2/movie/in_theaters?start=${event.start}&count=${event.count}`)
+    .then(function(res) {
+      console.log(res);
+      return res;
+    })
+    .catch(function(err) {
+      console.err(err);
+    });
+}
