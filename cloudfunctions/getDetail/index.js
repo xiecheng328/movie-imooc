be35fe046cf20a82c13c8b3fc8ae3b4138d91384
xiecheng328 <13136666328@163.com>
2019-05-15 12:34:23 +0800

    API增加apikey参数

--- cloudfunctions/getDetail/index.js
+++ cloudfunctions/getDetail/index.js
@@ -6,7 +6,7 @@ var rp = require('request-promise');
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  return rp(`http://api.douban.com/v2/movie/subject/${event.movieid}`)
+	return rp(`http://api.douban.com/v2/movie/subject/${event.movieid}?apikey=0df993c66c0c636e29ecbb5344252a4a`)
     .then(function (res) {
       return res;
     })
--- cloudfunctions/movielist/index.js
+++ cloudfunctions/movielist/index.js
@@ -9,12 +9,12 @@ var rp = require('request-promise');
 exports.main = async(event, context) => {
 
 
-  return rp(`http://api.douban.com/v2/movie/in_theaters?start=${event.start}&count=${event.count}`)
+	return rp(`http://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a&start=${event.start}&count=${event.count}`)
     .then(function(res) {
       console.log(res);
       return res;
     })
     .catch(function(err) {
-      console.err(err);
+      console.error(err);
     });
 }
